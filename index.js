1019c2ac807d17601f0d7abbf88d1919897a9fb9
Quentin Pigné <quentin.pigne@gmail.com>
2022-01-27 01:20:06 +0100

    add ability to configure starter using json file or user prompts

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ const path = require('path');
 
 const packageJson = require('./package.json');
 
-let projectName;
+const config = require('./lib/config');
 
-function init() {
+const argsAndOptions = () => {
+  let projectName;
   const program = new commander.Command(packageJson.name)
     .version(packageJson.version)
-    .argument('<project-name>', 'Name of the project (Used for directory name)')
+    .argument('<project-name>', 'The name of the project (used for directory name)')
     .action((name) => (projectName = name))
+    .option('-c, --config <config-file>', 'The path to the config file (optionnal)')
     .parse(process.argv);
 
-  createNodeApp();
+  return { projectName, ...program.opts() };
+};
+
+async function init() {
+  const options = argsAndOptions();
+  const starterConfig = await config.getConfig(options.config);
+
+  createNodeApp(options.projectName);
 }
 
-function createNodeApp() {
+function createNodeApp(projectName) {
   const root = path.resolve(projectName);
   const appName = path.basename(root);
 
